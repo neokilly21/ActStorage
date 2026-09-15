99107b727edcb8f529cfd6a92817be70399b2712
neokilly21 <neokilly21.2026@gmail.com>
2026-09-15 17:36:27 +0900

    샘플 프로그램 작성

---
+++ sample.py
@@ -0,0 +1,15 @@
+import requests
+
+# 이미지가 있는 url주소
+url = "https://news.nateimg.co.kr/orgImg/na/2026/09/12/8102624_high.jpg"
+
+# 해당 url로 서버에 요청
+img_response = requests.get(url)
+
+# 요청에 설공했다면,
+if img_response.status_code == 200:
+    #print( img_response.context )
+
+    print('================= [이미지 저장] =====================')
+    with open("test.jpg", "wb") as fp:
+        fp.write(img_response.content)
