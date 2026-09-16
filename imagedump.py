927aa11fda7d3acc712177e31d35f87f3582bf1c
neokilly21 <neokilly21.2026@gmail.com>
2026-09-16 14:15:33 +0900

    이미지 저장 함수 추가  / 시간 취득 방법

--- imagedump.py
+++ imagedump.py
@@ -1,5 +1,18 @@
 import requests
 import json
+from datetime import datetime
+
+# 이미지가 있는 image_url을 통해 file_name파일로 저장하는 함수
+def save_image(image_url, file_name):
+    img_response = requests.get(image_url)
+
+    #요청에 성공하면
+    if img_response.status_code == 200:
+        #파일 저장
+        with open(file_name, "wb") as fp:
+            fp.write(img_response.content)
+
+
 
 # 이미지 검색
 url = "https://dapi.kakao.com/v2/search/image"
@@ -20,7 +33,13 @@ else:   # 성공했다면
     count = 0
     for image_info in response.json()['documents']:
         print(f"[{count}th] image url =", image_info['image_url'])
-        # 저장할 이미지의 파일명 설정
+
+        # 저장할 이미지의 파일명 설정 , 검색어+시간+번호를 파일명으로 사용
         count = count + 1
-        
+        query = data["query"]     
+        timestamp = datetime.now().strftime("%Y.%m.%d_%H.%M.%S")
+        file_name = f"{query}_{timestamp}_{count}.jpg"
+        #이미지 저장
+        save_image(image_info['image_url'], file_name)
+
 
